7f73a498b784036139edb1dd929cc3bf6cf0748e
Willis Bridges <willisdbridges@gmail.com>
2023-10-19 22:55:26 -0700

    initial Main file to ensure API can start

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+# include routers
+# app.include_router(users.router, prefix="/users", tags=["users"])
+# app.include_router(bets.router, prefix="/bets", tags=["users"])
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # run app with uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
