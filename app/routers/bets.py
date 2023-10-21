9fba50e4b686248c715307d2f9390d4f1a68e763
Willis Bridges <willisdbridges@gmail.com>
2023-10-20 21:32:48 -0700

    updated router to reflect schema change

--- app/routers/bets.py
+++ app/routers/bets.py
@@ -1,15 +1,38 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database.database import get_db
-from database.models import Bet, BetCreate, BetUpdate, BetResolve
+from schemas.bets import Bet, BetCreate, BetUpdate
 
 router = APIRouter()
 
 
 @router.post("/bets/", response_model=Bet)
-def create_bet(bet: BetCreate, db: Session = Depends(get_db)):
+def create_bet(bet: BetCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """
-    endpoint for creating a new bet
+    Create a new bet
     """
-    # Bet creation logic goes here, create new bet in db?
-    return bet
+    db_bet = Bet(**bet.dict(), user_id=current_user.id)
+    db.add(db_bet)
+    db.commit()
+    db.refresh(db_bet)
+    return db_bet
+
+
+@router.get("/bets/{bet_id}", response_model=Bet)
+def read_bet(bet_id: int, db: Session = Depends(get_db)):
+    """
+    Get info for Bet ID
+    """
+    # TODO: Retrieve bet from database HERE
+    # return updated bet
+    return {"bet_id": bet_id, "updated_details": "updated bet details HERE"}
+
+
+@router.delete("/bets/{bet_id}", response_model=Bet)
+def delete_bet(bet_id: int, db: Session = Depends(get_db)):
+    """
+    Delete a specific bet by ID
+    """
+    # TODO: Delete bet from database HERE
+    # return deleted bet
+    return {"bet_id": bet_id, "status": "deleted"}
